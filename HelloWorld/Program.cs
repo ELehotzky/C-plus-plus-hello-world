38959aea5f42b3f3c5187e9b693f585d629750d4
ELehotzky <Lehotzky.Erica@gmail.com>
2018-12-23 17:54:56 -0600

    playing with C++ variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myValue;
+            string anotherValue;
+
+            myValue = "Value 1";
+            anotherValue = "Value 2";
+            Console.WriteLine(myValue);
+            Console.WriteLine(anotherValue);
+
+            anotherValue = myValue;
+            Console.WriteLine(anotherValue);
             Console.ReadLine();
+        }
+        static void WriteLine(string value)
+        {
+
         }
     }
 }
